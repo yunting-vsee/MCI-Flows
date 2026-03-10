0d3c9598e21c2cf8bf5ed245927dca76cd81776f
Yun-Ting Lien <yunting@vsee.com>
2026-03-10 01:13:58 -0400

    update for flyio

--- mci-flows/server.ts
+++ mci-flows/server.ts
@@ -275,9 +275,9 @@ async function startServer() {
     });
   }
 
-  const PORT = 62031;
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  const PORT = process.env.PORT || 8080;
+  app.listen(Number(PORT), '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
